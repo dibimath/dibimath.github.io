f023aabad4911a49013ec723b5cbb2245299c23f
Adam Streck <adam.streck@gmail.com>
2015-08-31 15:31:49 +0200

    Fix on location of project.

--- HGF_4_8_12/configure.js
+++ HGF_4_8_12/configure.js
@@ -1,1 +1,1 @@
-tremppi.setup = {"version": "0.4.1", "project_name": "HGF_4_8_12", "server_location": "localhost", "server_port": "8080"};
+tremppi.setup = {"server_port": "8080", "project_name": "HGF_4_8_12", "server_location": "localhost", "version": "0.4.1"};
